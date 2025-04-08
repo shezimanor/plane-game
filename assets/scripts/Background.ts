b8c24a57f646a7bf3090d9ce48ee2004b6b9b20a
Ryan Chen <y.f.chenroderick@gmail.com>
2025-04-08 22:41:58 +0800

    feat: 建立無限背景

---
+++ assets/scripts/Background.ts
@@ -0,0 +1,50 @@
+import { _decorator, CCInteger, Component, Node, UITransform } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Background')
+export class Background extends Component {
+  @property(Node)
+  private bgNode1: Node = null;
+  @property(Node)
+  private bgNode2: Node = null;
+
+  @property(CCInteger)
+  private _speed: number = 200;
+
+  private _height: number = 0;
+
+  start() {
+    this._height = this.bgNode1.getComponent(UITransform).height;
+  }
+
+  update(deltaTime: number) {
+    let position1 = this.bgNode1.position;
+    let position2 = this.bgNode2.position;
+    this.bgNode1.setPosition(
+      position1.x,
+      position1.y - this._speed * deltaTime,
+      position1.z
+    );
+    this.bgNode2.setPosition(
+      position2.x,
+      position2.y - this._speed * deltaTime,
+      position2.z
+    );
+    let currentPosition1 = this.bgNode1.position;
+    let currentPosition2 = this.bgNode2.position;
+    if (currentPosition1.y < -this._height) {
+      this.bgNode1.setPosition(
+        currentPosition1.x,
+        currentPosition2.y + this._height,
+        currentPosition1.z
+      );
+    }
+    if (currentPosition2.y < -this._height) {
+      this.bgNode2.setPosition(
+        currentPosition2.x,
+        currentPosition1.y + this._height,
+        currentPosition2.z
+      );
+    }
+  }
+}
