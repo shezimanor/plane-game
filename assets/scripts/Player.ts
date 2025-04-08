3ca9cdfb8581cd69cb216bd1ea643447e150f753
Ryan Chen <y.f.chenroderick@gmail.com>
2025-04-08 22:48:56 +0800

    feat: 建立玩家的物體 --- 建立持續動畫、腳本。

---
+++ assets/scripts/Player.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Player')
+export class Player extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
