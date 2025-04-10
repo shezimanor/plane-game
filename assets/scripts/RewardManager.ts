931e002e01441501522c41f7b3729e8ffca88871
Ryan Chen <y.f.chenroderick@gmail.com>
2025-04-10 13:26:21 +0800

    feat: 建立獎勵物品

---
+++ assets/scripts/RewardManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('RewardManager')
+export class RewardManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
